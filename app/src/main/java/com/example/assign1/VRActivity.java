aef080b3556925d69eb1db2e6b7de9a89ce69d17
Pushpinder1214 <71451404+Pushpinder1214@users.noreply.github.com>
2020-09-23 14:39:21 -0400

    First project Lab01

---
+++ app/src/main/java/com/example/assign1/VRActivity.java
@@ -0,0 +1,13 @@
+package com.example.assign1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VRActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_v_r);
+    }
+}
